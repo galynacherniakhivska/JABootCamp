adb706af32bc3088e6ee75d0be249c2c26e6d21f
galynacherniakhivska <linnulio@gmail.com>
2015-03-03 17:34:48 +0200

    Update Homework_4.js

--- Homework_4.js
+++ Homework_4.js
@@ -1,45 +1,77 @@
-var favBook = {
-
-};
+var SATATE_TITLE = 1;
+var STATE_NUM_PAGES = 2;
+var STATE_IS_INTERESTING = 3;
+var STATE_AUTHOR = 4;
+var STATE_YEAR_PUBLISHED = 5;
 
 var state = 1;
+var favBook = {
 
-function openUserInput () {
-process.stdin.setEncoding ('utf-8');
-process.stdin.resume();
-process.stdin.on ('data', onUserInput)
+	attributes: {},
+ 
+ };
+
+function openUserInput() {
+	process.stdin.setEncoding('utf-8');
+	process.stdin.resume();
+	process.stdin.on('data', onUserInput);	
 }
 
-function printBanner () {
-console.log ("****************************");
-console.log ("Hello, welcome to Book Worm!");
-console.log ("****************************");
-console.log ("Feel free to type 'exit' to quit");
-console.log ("To continue, please, type a title of your favorite book");
+function printBanner() {console.log ("\n****************************");
+	console.log ("Hello, welcome to Book Worm!");
+	console.log ("****************************");
+	console.log ("\nFeel free to type 'exit' to quit",
+				"\nTo continue, please, type a title of your favorite book",
+				"\nDo not forget to press ENTER to proceed after each answer given");
 }
 
-function onUserInput (input) {
-	input = normalizeInput(input);
+function onUserInput(input) {
 
-	if (input=="exit") {
-		console.log ("should exit");
-	} else {
-		if (state == 1) {//title
+	input = normalizeInput (input)
+	
+	if (input == "exit") {
+		process.exit();
+	} 
+	
+	switch (state) {
+		case SATATE_TITLE:
 			favBook.title = input;
-			state = 2;
-			console.log ("Please, enter the number of pages");
-			}
-			else if (state == 2) {//# of pages
-				favBook.numPages = +input;
-			}
-		}		
+			state = STATE_NUM_PAGES;
+			console.log("Please, enter the number of pages");
+		break;
+		case STATE_NUM_PAGES:
+			favBook.numPages = +input;
+			state = STATE_IS_INTERESTING;
+			//console.log(favBook.title, favBook.numPages);
+			console.log ("Is this book interesting? (Yes/No)");
+		break;
+		case STATE_IS_INTERESTING: 
+		favBook.isInteresting = input.toLowerCase ();
+		state = STATE_AUTHOR;
+		//console.log(favBook.isInteresting);
+		console.log ("Please, name the Author of the book");
+		break;
+		case STATE_AUTHOR:
+		favBook.attributes.author = input.toLowerCase ();
+		state = STATE_YEAR_PUBLISHED;
+		//console.log (favBook.attributes.author);
+		console.log ("Please, specify the year book was published");
+		break;
+		case STATE_YEAR_PUBLISHED:
+		favBook.attributes.yearPublished = +input;
+		//console.log (favBook.attributes.yearPublished);
+		console.log ("Well done! Please, check if all the information about the book is stored correctly:", 
+			"\n The Book Title Is:", favBook.title, 
+			"\n The Number Of Pages is:", favBook.numPages, 
+			"\n The Book Is Interesting:", favBook.isInteresting, 
+			"\n The Book Author is:", favBook.attributes.author,
+			"\n The Book was bublished in:", favBook.attributes.yearPublished + " year");
 		
-		console.log (favBook.title, favBook.numPages);
-	}	
+		}	
+}
+function normalizeInput(input) {
+	return input.trim();
 }
 
-function normalizeInput (input) {
-	return input.trim ();
-}
- onUserInput ();
- printBanner ();
+openUserInput();
+printBanner();
